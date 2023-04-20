f9cc1937105ff3011abadb35703517df520b2011
Allana Larissa <larissaleonel98@gmail.com>
2023-04-20 13:45:39 -0300

    Initial commit

---
+++ src/main/java/lari/springsecurityjwt/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package lari.springsecurityjwt.repository;
+
+
+
+
+import lari.springsecurityjwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+}
